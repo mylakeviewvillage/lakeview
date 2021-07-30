304b64375003bbc8379a1b7f52e711424a28d84a
daleSOCI <dale@socimedia.com>
2021-07-30 16:42:58 -0400

    finished responsive navigation

--- components/common/SiteHeader.js
+++ components/common/SiteHeader.js
@@ -2,6 +2,8 @@ import React from "react";
 import Link from 'next/link';
 import { useState } from "react";
 
+import styles from '../../styles/components/header.module.scss';
+
 const SiteHeader = ({ globalData, sitemapNode, page }) => {
     // get header data
     const { logo, mainMenu } = globalData.header;
@@ -9,52 +11,78 @@ const SiteHeader = ({ globalData, sitemapNode, page }) => {
     // set up href for internal links
     let href = "/pages/[...slug]";
 
+    const [openNav, setOpenNav] = useState(false);
     const [openSubNav, setOpenSubNav] = useState(0);
 
-    const hiddenSubNav = {
-        display: 'none'
+    const closeMenu = () => {
+        setOpenNav(false);
+        setOpenSubNav(0);
     }
-
-    const visibleSubNav = {
-        display: 'block'
-    }
-
-    console.log(openSubNav);
 
     return (
-        <header>
-            <img
-                src={logo.url}
-                alt={logo.label}
-                title={logo.siteName}
-            />
-            <nav>
-                <ul>
-                    {mainMenu.map((topLevelLink, topIndex) => {
-                        if (!topLevelLink.subNavLinks.totalCount) {
-                            return (
-                                <li key={`top-level-link-${topIndex}`}>
-                                    <Link href={topLevelLink.link.href}>
-                                        <a target={topLevelLink.link.target}>{topLevelLink.title}</a>
-                                    </Link>
-                                </li>)
-                        } else {
-                            return (
-                                <li key={`top-level-link-${topIndex}`}>
-                                    <button className="subMenuButton" onClick={() => setOpenSubNav(topIndex)}>{topLevelLink.title}</button>
-                                    <ul style={openSubNav === topIndex ? visibleSubNav : hiddenSubNav}>
-                                        {topLevelLink.subNavLinks.items.map((subNavLink, subIndex) => {
+        <header className={styles.header}>
+            <div className="container">
+                <div className={styles.content}>
+                    <div className={styles.logo}>
+                        <img src={logo.url} alt={logo.label} className="w-100" />
+                    </div>
+                    <button className="hamburger" onClick={() => setOpenNav(true)}>
+                        <div className="ham-bar top-bar"></div>
+                        <div className="ham-bar mid-bar"></div>
+                        <div className="ham-bar bot-bar"></div>
+                    </button>
+                    <nav className={openNav ? styles.active : styles.inactive}>
+                        <button className={styles.closeNav} onClick={closeMenu} aria-label="close navigation" />
+                        <div className={styles.topMenu}>
+                            <div className={`container ${styles.container}`}>
+                                <ul className={styles.topMenuList}>
+                                    {mainMenu.map((topLevelLink, topIndex) => {
+                                        if (!topLevelLink.subNavLinks.totalCount) {
                                             return (
-                                                <li key={`sub-nav-link-${topIndex}-${subIndex}`}>{subNavLink.fields.link.text}</li>
+                                                <li key={`top-level-link-${topIndex}`}>
+                                                    <Link href={topLevelLink.link.href}>
+                                                        <a target={topLevelLink.link.target}>{topLevelLink.title}</a>
+                                                    </Link>
+                                                </li>)
+                                        } else {
+                                            return (
+                                                <li key={`top-level-link-${topIndex}`}>
+                                                    <button className={styles.subMenuButton} onClick={() => setOpenSubNav(topIndex)}>{topLevelLink.title}</button>
+                                                    <div className={`${styles.subMenu} ${openSubNav === topIndex ? styles.active : styles.inactive}`}>
+                                                        <div className="container">
+                                                            <ul>
+                                                                <li>
+                                                                    <button onClick={() => setOpenSubNav(0)}>{topLevelLink.title}</button>
+                                                                </li>
+                                                                {topLevelLink.subNavLinks.items.map((subNavLink, subIndex) => {
+                                                                    return (
+                                                                        <li key={`sub-nav-link-${topIndex}-${subIndex}`}>
+                                                                            <Link href={'/'}>
+                                                                                <a>
+                                                                                    {subNavLink.fields.link.text}
+                                                                                </a>
+                                                                            </Link>
+                                                                        </li>
+                                                                    )
+                                                                })}
+                                                                <li>
+                                                                    <Link href="/contact">
+                                                                        <a>Contact</a>
+                                                                    </Link>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </li>
                                             )
-                                        })}
-                                    </ul>
-                                </li>
-                            )
-                        }
-                    })}
-                </ul>
-            </nav>
+                                        }
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </header>
     );
 };
@@ -87,7 +115,6 @@ SiteHeader.getCustomInitialProps = async function ({
             mainMenu = header.mainMenu.items;
             mainMenu.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
             mainMenu = mainMenu.map(link => link.fields);
-            console.log(mainMenu);
             // mainMenu = mainMenu.map(link => )
         } else {
             return null;
