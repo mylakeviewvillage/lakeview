7ff32a7503ee1823d46d9a6e0df0d91aff5e9f83
daleSOCI <dale@socimedia.com>
2021-08-10 16:42:06 -0400

    fatter content padding

--- components/agility-pageModules/ContactPanel.js
+++ components/agility-pageModules/ContactPanel.js
@@ -46,7 +46,7 @@ const ContactPanel = ({ module }) => {
     return (
         <section className={styles.contact_panel}>
             <div className="container">
-                <div className="content">
+                <div className="content fat">
                     <h2>{title}</h2>
                     <div className={styles.grid}>
                         <div className={styles.grid_left}>
