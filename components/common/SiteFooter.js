139679825c2b8eaab4278c8faa055c93e7e6d63f
daleSOCI <dale@socimedia.com>
2021-08-11 11:48:26 -0400

    global data retrived and sorted

--- components/common/SiteFooter.js
+++ components/common/SiteFooter.js
@@ -1,30 +1,63 @@
 import React from "react";
 
-const SiteFooter = () => {
+const SiteFooter = ({ globalData }) => {
 
-  return (
-    <footer>
-      Footer
-    </footer>
-  );
+    console.log(globalData);
+
+    return (
+        <footer>
+            Footer
+        </footer>
+    );
 };
 
 SiteFooter.getCustomInitialProps = async function ({
-  agility,
-  languageCode,
-  channelName,
+    agility,
+    languageCode,
+    channelName,
 }) {
 
-  let footer = null;
+    let logo;
+    let copyright;
+    let legalMenu;
+    let socialMenu;
+    let secondaryLinks;
+    let tertiaryLinks;
 
-  try {
+    try {
 
-  } catch (err) {
-    console.log("Global Footer error: ", err);
+        let footerContent = await agility.getContentList({
+            referenceName: "footer",
+            languageCode,
+            depth: 10,
+            expandAllContentLinks: true,
+            take: 10
+        });
 
-  }
+        if (footerContent.items && footerContent.items.length > 0) {
+            let footer = footerContent.items[0].fields;
+            logo = footer.logo;
+            copyright = footer.copyright;
+            legalMenu = footer.legalMenu.items;
+            legalMenu.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+            legalMenu = legalMenu.map(link => link.fields);
+            socialMenu = footer.socialMenu.items;
+            socialMenu.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+            socialMenu = socialMenu.map(link => link.fields);
+            secondaryLinks = footer.secondaryLinks.items;
+            secondaryLinks.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+            secondaryLinks = secondaryLinks.map(link => link.fields);
+            tertiaryLinks = footer.tertiaryLinks.items;
+            tertiaryLinks.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+            tertiaryLinks = tertiaryLinks.map(link => link.fields);
+        }
 
-  return { footer }
+    } catch (err) {
+        console.log("Global Footer error: ", err);
+
+    }
+
+    return { logo, copyright, legalMenu, socialMenu, secondaryLinks, tertiaryLinks }
 
 }
 
