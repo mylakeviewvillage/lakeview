7aa6e029a55dd2222b38c6a92304faff56795d14
daleSOCI <dale@socimedia.com>
2021-08-13 14:20:44 -0400

    accordion component finished / responsive

--- components/agility-pageModules/Accordion.js
+++ components/agility-pageModules/Accordion.js
@@ -1,16 +1,73 @@
+import RichTextArea from 'components/RichTextArea';
+import Link from 'next/link';
 import React from 'react';
+import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel, } from 'react-accessible-accordion';
 import styles from '../../styles/components/accordion.module.scss';
 
-const Accordion = () => {
+const AccordionSection = ({ module, customData }) => {
+
+    const { title, description, cTA } = module.fields;
+    const { accordionItems } = customData;
+
     return (
-        <section>
+        <section className={styles.accordion}>
             <div className="container">
                 <div className="content">
-                    Accordion
+                    <div className={styles.grid}>
+                        <div className={styles.grid_left}>
+                            {title && <h2>{title}</h2>}
+                            <hr />
+                            {description && <RichTextArea html={description} />}
+                            {cTA && <Link href={cTA.href}><a className="btn" target={cTA.target}>{cTA.text}</a></Link>}
+                        </div>
+                        <div className={styles.grid_right}>
+                            <Accordion allowMultipleExpanded allowZeroExpanded>
+                                {accordionItems.map((accordionItem, index) => (
+                                    <AccordionItem className={styles.collapsable_section} key={`collapsable-section-${index}`}>
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton className={styles.collapsable_section_title}>
+                                                <div className={styles.expand_icon}>
+                                                    <div className={styles.expand_icon_bar} />
+                                                    <div className={styles.expand_icon_bar} />
+                                                </div>
+                                                <h3>{accordionItem.title}</h3>
+                                                <h4>{accordionItem.subtitle}</h4>
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        <AccordionItemPanel className={styles.collapsable_section_hidden}>
+                                            <RichTextArea html={accordionItem.copy} />
+                                        </AccordionItemPanel>
+                                    </AccordionItem>
+                                ))}
+                            </Accordion>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
     );
 };
 
-export default Accordion;
+AccordionSection.getCustomInitialProps = async ({
+    agility,
+    languageCode,
+    channelName,
+    item
+}) => {
+    let accordionItems = null;
+    const api = agility;
+    let accordionItemsContent = await api.getContentList({
+        referenceName: item.fields.accordionItems.referencename,
+        languageCode: languageCode,
+        depth: 10,
+        expandAllContentLinks: true,
+        take: 10
+    });
+    accordionItemsContent.items.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+    accordionItems = accordionItemsContent.items.map(accordionItem => accordionItem.fields);
+    return {
+        accordionItems
+    }
+}
+
+export default AccordionSection;
