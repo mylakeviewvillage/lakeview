8a1d0afe736add2da5b4375945c069a9857f9503
daleSOCI <dale@socimedia.com>
2021-08-16 16:52:49 -0400

    separated sliders

--- components/agility-pageModules/LinkedSlider.js
+++ components/agility-pageModules/LinkedSlider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Link from 'next/link';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -14,14 +14,33 @@ const LinkedSlider = ({ module, customData }) => {
     const { titleOne, titleTwo, titleThree, layout, backgroundColor, copyColor } = module.fields;
     const { slides } = customData;
 
-    const settings = {
+    const settingsImageSlider = {
         dots: false,
         infinite: true,
         speed: 500,
         arrows: false,
+        // vertical: true
     };
 
-    const sliderRef = useRef();
+    const settingsInfoSlider = {
+        dots: false,
+        infinite: true,
+        speed: 700,
+        arrows: false,
+        adaptiveHeight: true,
+        cssEase: 'cubic-bezier(.63,-0.22,.44,1.21)'
+    };
+
+    const [imageNav, setImageNav] = useState(null);
+    const [infoNav, setInfoNav] = useState(null);
+
+    const imageSlider = useRef(null);
+    const infoSlider = useRef(null);
+
+    useEffect(() => {
+        setImageNav(imageSlider.current);
+        setInfoNav(infoSlider.current);
+    }, []);
 
     const ArrowLeft = ({ onClick }) => {
         return (
@@ -56,39 +75,55 @@ const LinkedSlider = ({ module, customData }) => {
                     )}
                 </div>
                 <div className="container full-width">
-                    <Slider {...settings} ref={sliderRef} className={styles.slider}>
-                        {slides.map((slide, index) => {
-                            return (
-                                <div>
-                                    <div
-                                        className={`${styles.slide} ${layout === 'left' ? styles.left : styles.right}`}
-                                        style={backgroundColor ? { background: `linear-gradient(to ${layout}, ${backgroundColor} 50%, transparent 50%)` } : null}
-                                        key={`linked-slider-slide-${index}`}>
+                    <div className={styles.sliders}>
+                        <div className={styles.image_slider}>
+                            <div className="w-100">
+                                <Slider {...settingsImageSlider} asNavFor={infoNav} ref={imageSlider}>
+                                    {slides.map((slide, index) => {
+                                        return (
+                                            <div>
+                                                <div
+                                                    className={`${styles.slide} ${layout === 'left' ? styles.left : styles.right}`}
+                                                    key={`linked-slider-slide-${index}`}>
 
-                                        <div className={styles.slide_image}>
-                                            <SEOImage img={slide.image} sizes={[1024, 500]} className="w-100" />
-                                        </div>
+                                                    <div className={styles.slide_image}>
+                                                        <SEOImage img={slide.image} sizes={[1024, 500]} className="w-100" />
+                                                    </div>
 
-                                        <div className={styles.slide_info} style={backgroundColor ? { backgroundColor } : null}>
-                                            <div className={`${styles.slide_info_content} ${copyColor === 'dark' ? styles.dark : styles.light}`}>
-                                                <div className={styles.navigation}>
-                                                    <ArrowLeft onClick={() => sliderRef.current.slickPrev()} />
-                                                    <span>{`${index + 1}/${slides.length}`}</span>
-                                                    <ArrowRight onClick={() => sliderRef.current.slickNext()} />
                                                 </div>
-                                                <h3>{slide.topTitle && <span className={`${styles.top_title} ${copyColor === 'dark' ? styles.dark : styles.light}`}>{slide.topTitle}<br /></span>}{slide.slideTitle}</h3>
-                                                <RichTextArea html={slide.slideCopy} />
-                                                <Link href={slide.cTA.href}>
-                                                    <a target={slide.cTA.target} className={`btn solid-white`}>{slide.cTA.text}</a>
-                                                </Link>
                                             </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </Slider>
+                                        )
+                                    })}
+                                </Slider>
+                            </div>
+                        </div>
+                        <div className={styles.info_slider} style={backgroundColor ? { backgroundColor: backgroundColor } : null}>
+                            <div className="w-100">
+                                <Slider {...settingsInfoSlider} asNavFor={imageNav} ref={infoSlider}>
+                                    {slides.map((slide, index) => {
+                                        return (
+                                            <div>
+                                                <div className={styles.slide_info} style={backgroundColor ? { backgroundColor } : null}>
+                                                    <div className={`${styles.slide_info_content} ${copyColor === 'dark' ? styles.dark : styles.light}`}>
+                                                        <div className={styles.navigation}>
+                                                            <ArrowLeft onClick={() => infoSlider.current.slickPrev()} />
+                                                            <span>{`${index + 1}/${slides.length}`}</span>
+                                                            <ArrowRight onClick={() => infoSlider.current.slickNext()} />
+                                                        </div>
+                                                        <h3>{slide.topTitle && <span className={`${styles.top_title} ${copyColor === 'dark' ? styles.dark : styles.light}`}>{slide.topTitle}<br /></span>}{slide.slideTitle}</h3>
+                                                        <RichTextArea html={slide.slideCopy} />
+                                                        <Link href={slide.cTA.href}>
+                                                            <a target={slide.cTA.target} className={`btn solid-white`}>{slide.cTA.text}</a>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </Slider>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
