8193c28eac98de884e21c659d3e5e88d333b0b22
daleSOCI <dale@socimedia.com>
2021-08-17 14:52:18 -0400

    helper function for repetitive content lists

---
+++ helpers/getContentList.js
@@ -0,0 +1,16 @@
+export async function getContentList(agility, referenceName, languageCode) {
+    let contentList = null;
+    const api = agility;
+    let contentListRes = await api.getContentList({
+        referenceName,
+        languageCode,
+        depth: 10,
+        expandAllContentLinks: true,
+        take: 10
+    });
+    contentListRes.items.sort((a, b) => (a.properties.itemOrder > b.properties.itemOrder) ? 1 : -1);
+    contentList = contentListRes.items.map(item => item.fields);
+    return {
+        contentList
+    }
+}
