cde39e7bb02f291a32400d8b7ca4acaf669c924c
daleSOCI <dale@socimedia.com>
2021-08-17 15:16:46 -0400

    add titles for banner (reintro top padding)

--- components/agility-pageModules/ImageBanner.js
+++ components/agility-pageModules/ImageBanner.js
@@ -1,12 +1,20 @@
+import SectionTitle from 'components/SectionTitle';
 import SEOImage from 'components/SEOImage';
 import React from 'react';
+import styles from '../../styles/components/banner.module.scss';
 
 const Banner = ({ module }) => {
+
+    const { image, titleOne, titleTwo, titleThree } = module.fields;
+
     return (
-        <section className="banner">
-            <div className="container full-width">
-                <div className="content no-top">
-                    <SEOImage img={module.fields.image} sizes={[500, 1024, 1920]} className="w-100" />
+        <section className={styles.banner}>
+            <div className="content no-top">
+                <div className="container">
+                    {titleOne && <SectionTitle titleOne={titleOne} titleTwo={titleTwo} titleThree={titleThree} />}
+                </div>
+                <div className="container full-width">
+                    <SEOImage img={image} sizes={[500, 1024, 1920]} className="w-100" />
                 </div>
             </div>
         </section>
