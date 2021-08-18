acc4bfd1f84f562ce465af39cdb482f4e24dea77
daleSOCI <dale@socimedia.com>
2021-08-18 10:25:24 -0400

    add webp support to images

--- components/SEOImage.js
+++ components/SEOImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SEOImage = ({ img, sizes, className }) => {
-    const srcSet = sizes.map(size => `${img.url}?w=${size} ${size}w, `);
+    const srcSet = sizes.map(size => `${img.url}?format=auto&w=${size} ${size}w, `);
     return (
         <img src={img.url} srcSet={srcSet} alt={img.label} className={className} />
     );
