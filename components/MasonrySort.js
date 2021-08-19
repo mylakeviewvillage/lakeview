99f3c7aa472c95c6509e1d3bd553f51dbe07540b
daleSOCI <dale@socimedia.com>
2021-08-19 13:57:15 -0400

    Sort desktop styled

--- components/MasonrySort.js
+++ components/MasonrySort.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from 'styles/components/masonry-sort.module.scss';
 
 const MasonrySort = ({ filterCards }) => {
+
+    const [activeFilter, setActiveFilter] = useState('All');
+
+    const onFilterChange = (filter) => {
+        filterCards(filter);
+        setActiveFilter(filter);
+    }
+
     return (
-        <div className="container">
-            <div className={styles.filters}>
-                <button onClick={() => filterCards('All')}>All</button>
-                <button onClick={() => filterCards('News')}>News</button>
-                <button onClick={() => filterCards('Community Relations')}>Community Relations</button>
-                <button onClick={() => filterCards('Discovery Centre')}>Discovery Centre</button>
-                <button onClick={() => filterCards('Placemaking')}>Placemaking</button>
-                <button onClick={() => filterCards('Planning & Project')}>Planning & Project</button>
-                <button onClick={() => filterCards('Transit & Trails')}>Transit & Trails</button>
-            </div>
+        <div className={styles.filters}>
+            <button className={activeFilter === 'All' ? styles.active : null} onClick={() => onFilterChange('All')}>All</button>
+            <button className={activeFilter === 'News' ? styles.active : null} onClick={() => onFilterChange('News')}>News</button>
+            <button className={activeFilter === 'Community Relations' ? styles.active : null} onClick={() => onFilterChange('Community Relations')}>Community Relations</button>
+            <button className={activeFilter === 'Discovery Centre' ? styles.active : null} onClick={() => onFilterChange('Discovery Centre')}>Discovery Centre</button>
+            <button className={activeFilter === 'Placemaking' ? styles.active : null} onClick={() => onFilterChange('Placemaking')}>Placemaking</button>
+            <button className={activeFilter === 'Planning & Project' ? styles.active : null} onClick={() => onFilterChange('Planning & Project')}>Planning & Project</button>
+            <button className={activeFilter === 'Transit & Trails' ? styles.active : null} onClick={() => onFilterChange('Transit & Trails')}>Transit & Trails</button>
         </div>
     );
 };
