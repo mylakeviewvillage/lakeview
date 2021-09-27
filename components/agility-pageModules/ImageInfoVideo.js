4267bb3a910edb68c46cd64f1e7cffc1f2fb0d0d
daleSOCI <dale@socimedia.com>
2021-09-27 16:56:22 -0400

    add all new components

---
+++ components/agility-pageModules/ImageInfoVideo.js
@@ -0,0 +1,32 @@
+import RichTextArea from 'components/RichTextArea';
+import SectionTitle from 'components/SectionTitle';
+import SEOImage from 'components/SEOImage';
+import React from 'react';
+import styles from '../../styles/components/image-info.module.scss';
+
+const ImageInfoVideo = ({ module }) => {
+    const { titleOne, titleTwo, titleThree, image, topTitle, mainTitle, copy, videoID, backgroundColor, textColor } = module.fields;
+    return (
+        <section>
+            <div className="content">
+                <div className="container">
+                    <SectionTitle titleOne={titleOne} titleTwo={titleTwo} titleThree={titleThree} />
+                </div>
+                <div className="container full-width">
+                    <div className={styles.image_info} style={backgroundColor ? { background: `linear-gradient(to left, ${backgroundColor} 75%, transparent 75%)` } : null}>
+                        <div className={styles.image}>
+                            <SEOImage img={image} className="w-100" sizes={[700, 600]} />
+                        </div>
+                        <div className={`${styles.info} ${textColor === 'Light' ? styles.light : styles.dark}`}>
+                            <h3>{topTitle && <span>{topTitle}<br /></span>}{mainTitle}</h3>
+                            {copy && <RichTextArea html={copy} />}
+                            {videoID && <button className={`btn ${textColor === 'Light' ? 'white' : ''}`}>Watch Video</button>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ImageInfoVideo;
