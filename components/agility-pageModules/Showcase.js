843cc96dd4fad505db06710dd97172c255bf5ced
daleSOCI <dale@socimedia.com>
2021-09-30 16:38:29 -0400

    fix empty image (conditional)

--- components/agility-pageModules/Showcase.js
+++ components/agility-pageModules/Showcase.js
@@ -22,7 +22,7 @@ const Showcase = ({ module }) => {
                             </div>
                         </div>
                         <div className={styles.grid_right}>
-                            <SEOImage img={image} className="w-100" sizes={[1200, 800, 400]} />
+                            {image && <SEOImage img={image} className="w-100" sizes={[1200, 800, 400]} />}
                         </div>
                     </div>
                 </div>
