14f9edadb9b60ca0a7dcc09ee57d7f2676191979
daleSOCI <dale@socimedia.com>
2021-10-01 11:47:27 -0400

    three sixty infinite

--- components/agility-pageModules/ThreeSixtySlider.js
+++ components/agility-pageModules/ThreeSixtySlider.js
@@ -29,18 +29,18 @@ const ThreeSixtySlider = ({ module }) => {
 
     const settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 500,
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         className: [styles.slider],
         arrows: false,
+        dots: true,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 1,
-                    dots: true
                 }
             }
         ]
