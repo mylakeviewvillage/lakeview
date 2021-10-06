b6c267bf295c8dda7f46e301f87add7bf5642e23
daleSOCI <dale@socimedia.com>
2021-10-06 15:12:06 -0400

    set a fallback default 2:1 image ratio

--- components/SEOImage.js
+++ components/SEOImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SEOImage = ({ img, sizes, className }) => {
     const srcSet = sizes.map(size => `${img.url}?format=auto&w=${size} ${size}w, `);
     return (
-        <img src={img.url} width={img.width} height={img.height} srcSet={srcSet} alt={img.label ? img.label : ''} className={className} />
+        <img src={img.url} width={img.width ? img.width : 2} height={img.height ? img.height : 1} srcSet={srcSet} alt={img.label ? img.label : ''} className={className} />
     );
 };
 
