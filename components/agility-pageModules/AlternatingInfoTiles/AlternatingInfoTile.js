7fef50d263623c3d7448277fbcf93c50447b9aea
unclebreakfast <unclebreakfast@users.noreply.github.com>
2021-10-19 12:02:15 -0400

    Added video to alternating tile component

--- components/agility-pageModules/AlternatingInfoTiles/AlternatingInfoTile.js
+++ components/agility-pageModules/AlternatingInfoTiles/AlternatingInfoTile.js
@@ -1,16 +1,32 @@
 import RichTextArea from 'components/RichTextArea';
 import SEOImage from 'components/SEOImage';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { Fade } from 'react-awesome-reveal';
 import styles from 'styles/components/alternating-info-tiles.module.scss';
+import ModalVideo from 'react-modal-video';
 
 const AlternatingInfoTile = ({ tile }) => {
+
+    const [modal, setModal] = useState(false);
+    const [videoID, setVideoID] = useState('');
+
+    const openModal = (videoID) => {
+        setModal(true);
+        setVideoID(videoID);
+    }
+
+    console.log(tile);
 
     return (
         <div className={styles.tile}>
             <div className={styles.image}>
-                <SEOImage img={tile.image} sizes={[300, 600]} className="w-100" />
+                {tile.videoID && (
+                    <button onClick={() => openModal(videoID)}>
+                        <SEOImage img={tile.image} sizes={[300, 600]} className="w-100" />
+                    </button>
+                )}
+                {!tile.videoID && (<SEOImage img={tile.image} sizes={[300, 600]} className="w-100" />)}
             </div>
             <div className={styles.copy}>
                 <p className={styles.minor}>{tile.subtitle}</p>
@@ -32,10 +48,16 @@ const AlternatingInfoTile = ({ tile }) => {
                     </Fade>
                 </ul>
 
-                {tile.cTA && (
+                {tile.cTA && !tile.videoID && (
                     <Link href={tile.cTA.href}><a className="btn" target={tile.cTA.target}>{tile.cTA.text}</a></Link>
                 )}
+                {tile.videoID && (
+                    <div>
+                        <button className={`btn ${styles.cta}`} onClick={() => openModal(tile.videoID)}>Play Video</button>
+                    </div>
+                )}
             </div>
+            <ModalVideo channel='youtube' autoplay={true} isOpen={modal} videoId={videoID} onClose={() => setModal(false)} />
         </div>
     );
 };
