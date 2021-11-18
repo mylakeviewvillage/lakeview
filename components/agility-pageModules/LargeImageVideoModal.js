8dd32f490b2d070f910a200bc06c9c5ceeded9c4
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2021-11-18 09:42:15 -0500

    Vimeo Option on VideoModal

--- components/agility-pageModules/LargeImageVideoModal.js
+++ components/agility-pageModules/LargeImageVideoModal.js
@@ -6,7 +6,7 @@ import styles from 'styles/components/large-image-video-modal.module.scss';
 
 const LargeImageVideoModal = ({ module }) => {
 
-    const { titleOne, titleTwo, titleThree, image, youtubeVideoID } = module.fields;
+    const { titleOne, titleTwo, titleThree, image, videoID, isVimeo } = module.fields;
 
     const [openModal, setOpenModal] = useState(false);
 
@@ -21,7 +21,7 @@ const LargeImageVideoModal = ({ module }) => {
                             <SEOImage img={image} sizes={[1400, 1024, 768, 400]} className="w-100" />
                         </div>
                     )}
-                    <ModalVideo channel='youtube' autoplay={true} isOpen={openModal} videoId={youtubeVideoID} onClose={() => setOpenModal(false)} />
+                    <ModalVideo channel={isVimeo ? 'vimeo':'youtube'} autoplay={true} isOpen={openModal} videoId={videoID} onClose={() => setOpenModal(false)} />
                 </div>
             </div>
         </section>
