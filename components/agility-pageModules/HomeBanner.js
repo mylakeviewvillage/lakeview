329a9607f843a6acede7304ff21a55e8d21debe3
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2021-11-19 14:06:33 -0500

    Home Banner Updates

--- components/agility-pageModules/HomeBanner.js
+++ components/agility-pageModules/HomeBanner.js
@@ -40,12 +40,12 @@ const HomeBanner = ({ module }) => {
             <div className="container">
                 <div className="content no-top">
                     <div className={styles.partners}>
-                        <a href="https://www.branthaven.com/"><img src="/img/homebanner/logo-BH.svg" /></a>
-                        <a href="https://caivan.com/communities/greater-toronto-area/lakeview-village/"><img src="/img/homebanner/logo-CAIVAN.svg" /></a>
-                        <a href="https://www.greenparkgroup.ca/"><img src="/img/homebanner/logo-GP.svg" /></a>
-                        <a href="https://www.decohomes.ca/"><img src="/img/homebanner/logo-DECO.svg" /></a>
-                        <a href="https://www.opushomes.com/"><img src="/img/homebanner/logo-OPUS.svg" /></a>
-                        <a href="https://www.tridel.com/"><img src="/img/homebanner/logo-TRIDEL.svg" /></a>
+                        <a href="https://www.branthaven.com/" target="_blank"><img src="/img/homebanner/logo-BH.svg" /></a>
+                        <a href="https://caivan.com/communities/greater-toronto-area/lakeview-village/" target="_blank"><img src="/img/homebanner/logo-CAIVAN.svg" /></a>
+                        <a href="https://www.greenparkgroup.ca/" target="_blank"><img src="/img/homebanner/logo-GP.svg" /></a>
+                        <a href="https://www.decohomes.ca/" target="_blank"><img src="/img/homebanner/logo-DECO.svg" /></a>
+                        <a href="https://www.opushomes.com/" target="_blank"><img src="/img/homebanner/logo-OPUS.svg" /></a>
+                        <a href="https://www.tridel.com/" target="_blank"><img src="/img/homebanner/logo-TRIDEL.svg" /></a>
                     </div>
                 </div>
             </div>
