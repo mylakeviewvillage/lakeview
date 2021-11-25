61a96eb87fa7db6bd2cca57b48113da4b4afa6f4
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2021-11-25 08:58:41 -0500

    Added FB domain verification meta field

--- components/common/SEO.js
+++ components/common/SEO.js
@@ -16,6 +16,7 @@ const SEO = ({ title, description, keywords, ogImage, metaHTML }) => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+      <meta name="facebook-domain-verification" content="jsl91nobjcd5kfhiebxvue4cgdg6w1" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
         rel="stylesheet"
