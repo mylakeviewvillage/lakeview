e8ce76dce267e64223cd3302d82449c7f746a1a4
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2022-01-26 13:09:08 -0500

    Added more returns

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,15 +3,21 @@ import { NextResponse } from 'next/server';
 const Middleware = (req) => {
 
   if (req?.page?.params?.slug) {
+    console.log(req.page.params);
     if (req.page.params.slug[0] !== 'img') {
       console.log('running page request');
       if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
         return NextResponse.next();
 
       return NextResponse.redirect(`${req.nextUrl.origin}${req.nextUrl.pathname.toLowerCase()}`);
+    }else{
+      return NextResponse.next();
     }
+  }else{
+    return NextResponse.next();
   }
 
+  
 };
 
 export default Middleware;
