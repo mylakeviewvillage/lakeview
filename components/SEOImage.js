57f214c1590a51db26c5c8bc298dfb9728903021
daleSOCI <dale@socimedia.com>
2022-02-16 10:45:39 -0500

    update SEO images

--- components/SEOImage.js
+++ components/SEOImage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SEOImage = ({ img, sizes, className }) => {
-    const srcSet = sizes.map(size => `${img.url}?format=auto&w=${size} ${size}w, `);
+const SEOImage = ({ img, sizes, className, width, height }) => {
+
+    const [svg, setSvg] = useState(false);
+
+    useEffect(() => {
+        if (img.url.includes('.svg')) {
+            setSvg(true);
+        }
+    }, [img]);
+
+    let srcSet = svg ?
+        img.url :
+        sizes.map(size => `${img?.url.replace(/ /g, '%20')}?format=auto&w=${size} ${size}w, `);
+
     return (
-        <img src={img.url} width={img.width ? img.width : 2} height={img.height ? img.height : 1} srcSet={srcSet} alt={img.label ? img.label : ''} className={className} />
+        <>
+            {img && (
+                <img src={img.url} srcSet={srcSet} alt={img.label || ''} width={width ?? img.width ?? 1} height={height ?? img.height ?? 1} className={className} />
+            )}
+        </>
     );
 };
 
