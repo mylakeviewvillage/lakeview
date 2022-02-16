0682ce9494c2d1866ff9103641488567b2c380a7
daleSOCI <dale@socimedia.com>
2022-02-16 16:55:41 -0500

    preconnect google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Favicon from 'components/common/Favicon'
-import SEO from 'components/common/SEO'
 import Trackers from 'components/common/Trackers'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
@@ -15,6 +14,7 @@ class MyDocument extends Document {
                 <Head>
                     <Favicon />
                     <Trackers google="UA-117378406-1" facebook="416073699992441" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com"></link>
                 </Head>
                 <body>
                     <Main />
