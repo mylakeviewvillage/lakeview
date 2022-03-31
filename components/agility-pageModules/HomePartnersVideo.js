c12022b3f7af2efcec2f8607af5d0bde47607318
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2022-03-31 15:47:05 -0400

    Home Partners

--- components/agility-pageModules/HomePartnersVideo.js
+++ components/agility-pageModules/HomePartnersVideo.js
@@ -17,23 +17,36 @@ const HomePartnersVideo = ({ module }) => {
                 </div>
                 <div className={styles.partners}>
                     <div className={styles.partner}>
-                        <a href="https://www.branthaven.com/communities/lakeview-village/" aria-label='navigates to branthaven website' target="_blank"><img src="/img/builderbanner/logo-bh-wht.svg" width={2} height={1} alt="Branthaven" /></a>
-                        <a href="https://www.branthaven.com/communities/lakeview-village/" aria-label='navigates to branthaven website' target="_blank"><img src="/img/arrow-right-long.svg" width={2} height={1} alt="Branthaven" /></a>
+                        <a href="https://www.branthaven.com/communities/lakeview-village/" aria-label='navigates to branthaven website' target="_blank">
+                            <img src="/img/builderbanner/logo-bh-wht.svg" width={2} height={1} alt="Branthaven" className={styles.logo_branthaven} />
+                        </a>
+                        <a href="https://www.branthaven.com/communities/lakeview-village/" aria-label='navigates to branthaven website' target="_blank">
+                            <img src="/img/arrow-right-long.svg" width={2} height={1} alt="Branthaven" className={styles.arrow_right} />
+                        </a>
                     </div>
                     <div className={styles.partner}>
-                        <a href="https://caivan.com/greater-toronto-area/lakeview-village" target="_blank" aria-label='navigates to vaivan website'><img src="/img/builderbanner/logo-caivan-wht.svg" width={2} height={1} alt="caivan" /></a>
+                        <a href="https://caivan.com/greater-toronto-area/lakeview-village" target="_blank" aria-label='navigates to vaivan website'>
+                            <img src="/img/builderbanner/logo-caivan-wht.svg" width={2} height={1} alt="caivan" className={styles.logo_caivan} />
+                        </a>
+                        <a href="https://caivan.com/greater-toronto-area/lakeview-village" target="_blank" aria-label='navigates to vaivan website'>
+                            <img src="/img/arrow-right-long.svg" width={2} height={1} alt="caivan" className={styles.arrow_right} />
+                        </a>
                     </div>
                     <div className={styles.partner}>
                         <a href="https://www.greenparkgroup.ca/" target="_blank" aria-label='navigates to greenpark group website'><img src="/img/builderbanner/logo-gp-wht.svg" width={2} height={1} alt="greenpark group" /></a>
+                        <a href="https://www.greenparkgroup.ca/" target="_blank" aria-label='navigates to greenpark group website'><img src="/img/arrow-right-long.svg" width={2} height={1} alt="greenpark group" className={styles.arrow_right} /></a>
                     </div>
                     <div className={styles.partner}>
                         <a href="https://www.decohomes.ca/" target="_blank" aria-label='navigates to decohomes website'><img src="/img/builderbanner/logo-deco-wht.svg" width={2} height={1} alt="deco" /></a>
+                        <a href="https://www.decohomes.ca/" target="_blank" aria-label='navigates to decohomes website'><img src="/img/arrow-right-long.svg" width={2} height={1} alt="deco" className={styles.arrow_right} /></a>
                     </div>
                     <div className={styles.partner}>
                         <a href="https://www.opushomes.com/" target="_blank" aria-label='navigates to opus homes website'><img src="/img/builderbanner/logo-opus-wht.svg" width={2} height={1} alt="opus homes" /></a>
+                        <a href="https://www.opushomes.com/" target="_blank" aria-label='navigates to opus homes website'><img src="/img/arrow-right-long.svg" width={2} height={1} alt="opus homes" className={styles.arrow_right} /></a>
                     </div>
                     <div className={styles.partner}>
-                        <a href="https://www.tridel.com/lakeviewvillagecondos" target="_blank" aria-label='navigates to tridel website'><img src="/img/builderbanner/logo-tridel-ltblue.svg" width={2} height={1} alt="Tridel - Built for life" /></a>
+                        <a href="https://www.tridel.com/lakeviewvillagecondos" target="_blank" aria-label='navigates to tridel website'><img src="/img/builderbanner/logo-tridel-ltblue.svg" width={2} height={1} alt="Tridel - Built for life" className={styles.logo_tridel} /></a>
+                        <a href="https://www.tridel.com/lakeviewvillagecondos" target="_blank" aria-label='navigates to tridel website'><img src="/img/arrow-right-long-dark.svg" width={2} height={1} alt="Tridel - Built for life" className={styles.arrow_right} /></a>
                     </div>
                 </div>
             </div>
