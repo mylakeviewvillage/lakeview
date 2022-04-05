49cc6623761857745b4674df0b4211217579e4f9
unclebreakfast <74007257+socimedia@users.noreply.github.com>
2022-04-05 14:10:59 -0400

    remove arrows for mobile

--- components/agility-pageModules/HomePartnersVideo.js
+++ components/agility-pageModules/HomePartnersVideo.js
@@ -14,7 +14,7 @@ const HomePartnersVideo = ({ module }) => {
                 <div className={styles.partners_video}>
                     <Fade direction="up">
                         <div className={styles.embed_container}>
-                            <iframe src="https://player.vimeo.com/video/655037869?h=c6db007fe5&color=ef0800&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>
+                            <iframe src="https://player.vimeo.com/video/655037869?h=c6db007fe5&color=ef0800&title=0&byline=0&portrait=0" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </Fade>
                 </div>
